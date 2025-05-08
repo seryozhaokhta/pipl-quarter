6fa92308383c246a8f4bfe8ab05258212ff75fcb
seryozhaokhta <93487244+seryozhaokhta@users.noreply.github.com>
2025-05-08 07:48:35 +0400

    fix

--- composables/useGsapTimeline.ts
+++
@@ -1,8 +0,0 @@
-import gsap from 'gsap'
-import { onUnmounted } from 'vue'
-
-export function useGsapTimeline(config: gsap.TimelineVars = {}) {
-    const tl = gsap.timeline({ paused: true, ...config })
-    onUnmounted(() => tl.kill())
-    return tl
-}
